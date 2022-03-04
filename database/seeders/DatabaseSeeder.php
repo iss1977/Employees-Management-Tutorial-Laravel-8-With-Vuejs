4de53677c132a4767b61da8eca894ad80844caac
iss1977 <ioan.szucs@gmail.com>
2022-03-04 20:51:26 +0100

    setting up user seeder with faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +17,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = Faker::create();
+    	foreach (range(1,100) as $index) {
+	        DB::table('users')->insert([
+	            'username' => $faker->unique()->userName,
+                'first_name' => $faker->firstName,
+                'last_name' => $faker->lastName,
+	            'email' => $faker->email,
+	            'password' => bcrypt('secret'),
+	        ]);
+	    }
     }
 }
